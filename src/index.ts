afc3fa99ab59a882aa361775915fe2ba0499664e
Sylvain <sylvain.touchard@edithdigital.com>
2021-11-22 00:20:37 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import InterfaceComponent from './interface.vue';
+
+export default {
+	id: 'uuid-file-image',
+	name: 'UUID File Image',
+	description: 'Select image from Library in a UUID string field',
+	icon: 'box',
+	component: InterfaceComponent,
+	types: ['uuid'],
+};
