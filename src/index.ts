8d188f342bd24a6dc7436e29e0e1dc61fd37508b
Andreas Holmgrein <a.holmgrein@zaun-idee.de>
2023-07-27 22:28:25 +0200

    Implement extensions SDK for interface definitions
    Introduced the use of 'defineInterface' method from the Directus extensions SDK in the interface definition for 'uuid-file-image'. This change is necessary to provide a clearer structure and easier maintenance of code. The options parameter has been set as null for now.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
 
-export default {
+export default defineInterface({
 	id: 'uuid-file-image',
 	name: 'UUID File Image',
-	description: 'Select image from Library in a UUID string field',
 	icon: 'box',
+	description: 'Select image from Library in a UUID string field',
 	component: InterfaceComponent,
+	options: null,
 	types: ['uuid'],
-};
+});
